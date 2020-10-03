b027dfbec4120864d3a635946c9a694595c25984
Ivan Kashin <barbos1701@gmail.com>
2020-10-03 09:25:41 +0300

    Добавляет пути

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen";
 
 const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen errorsCount={errorsCount}/>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact />
+        <Route path="/login" exact />
+        <Route path="/result" exact />
+        <Route path="/lose" exact />
+        <Route path="/dev-artist" exact />
+        <Route path="/dev-genre" exact />
+      </Switch>
+    </BrowserRouter>
+    // <WelcomeScreen errorsCount={errorsCount}/>
   );
 };
 
