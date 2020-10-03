64f224baa682516c2002e9ae8e275e4f0bd02750
Ivan Kashin <barbos1701@gmail.com>
2020-10-03 09:52:52 +0300

    Подключает новые компоненты в роутинг и правит верстку для jsx

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen";
+import AuthScreen from "../auth-screen/auth-screen";
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen";
+import GenreQuestionScreen from "../genre-question-screen/genre-question-screen";
+import WinScreen from "../win-screen/win-screen";
+import GameOverScreen from "../game-over-screen/game-over-screen";
 
 const App = (props) => {
   const {errorsCount} = props;
@@ -9,15 +14,26 @@ const App = (props) => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact />
-        <Route path="/login" exact />
-        <Route path="/result" exact />
-        <Route path="/lose" exact />
-        <Route path="/dev-artist" exact />
-        <Route path="/dev-genre" exact />
+        <Route path="/" exact>
+          <WelcomeScreen errorsCount={errorsCount}/>
+        </Route>
+        <Route path="/dev-artist" exact>
+          <ArtistQuestionScreen />
+        </Route>
+        <Route path="/dev-genre" exact>
+          <GenreQuestionScreen />
+        </Route>
+        <Route path="/login" exact>
+          <AuthScreen />
+        </Route>
+        <Route path="/result" exact>
+          <WinScreen />
+        </Route>
+        <Route path="/lose" exact>
+          <GameOverScreen />
+        </Route>
       </Switch>
     </BrowserRouter>
-    // <WelcomeScreen errorsCount={errorsCount}/>
   );
 };
 
