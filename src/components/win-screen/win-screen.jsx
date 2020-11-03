1e516de448009c648f74fd34543b8e96aefc7d69
Ivan Kashin <barbos1701@gmail.com>
2020-11-03 09:48:21 +0300

    Обновляет взаимодействие с хранилищем

--- src/components/win-screen/win-screen.jsx
+++ src/components/win-screen/win-screen.jsx
@@ -35,9 +35,9 @@ WinScreen.propTypes = {
   resetGameAction: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = (state) => ({
-  questionsCount: state.step,
-  mistakesCount: state.mistakes,
+const mapStateToProps = ({GAME}) => ({
+  questionsCount: GAME.step,
+  mistakesCount: GAME.mistakes,
 });
 
 const mapDispatchToProps = (dispatch) => ({
