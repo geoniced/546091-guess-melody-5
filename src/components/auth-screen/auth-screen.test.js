362a099eb5fcf916e837aba9cc98dc5cace6cfb2
Ivan Kashin <barbos1701@gmail.com>
2020-11-15 08:18:37 +0300

    Добавляет снепшот тесты для компонентов

---
+++ src/components/auth-screen/auth-screen.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import {AuthScreen} from "./auth-screen";
+
+const noop = () => {};
+
+it(`AuthScreen renders correctly`, () => {
+  const tree = renderer
+    .create(
+        <AuthScreen
+          onReplayButtonClick={noop}
+          onSubmit={noop}
+        />
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
