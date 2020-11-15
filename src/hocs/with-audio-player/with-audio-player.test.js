a74f93365300a1333f6d47ecce7d513c3aade419
Ivan Kashin <barbos1701@gmail.com>
2020-11-15 08:47:08 +0300

    Добавляет снепшот тесты ХОКам

---
+++ src/hocs/with-audio-player/with-audio-player.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import PropTypes from "prop-types";
+import withAudioPlayer from "./with-audio-player";
+
+const MockComponent = (props) => {
+  const {children} = props;
+
+  return (
+    <div>
+      {children}
+    </div>
+  );
+};
+
+MockComponent.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
+};
+
+const MockComponentWrapped = withAudioPlayer(MockComponent);
+
+it(`withAudioPlayer renders correctly`, () => {
+  const tree = renderer
+    .create(
+        <MockComponentWrapped>
+          <React.Fragment />
+        </MockComponentWrapped>
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
