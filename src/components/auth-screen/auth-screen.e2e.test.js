98c25ed5b96c1f847e3e4b5dcdfc24ba674bdba8
Ivan Kashin <barbos1701@gmail.com>
2020-11-16 19:54:08 +0300

    Добавляет ензим е2е тесты

---
+++ src/components/auth-screen/auth-screen.e2e.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Enzyme, {shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import {AuthScreen} from "./auth-screen";
+
+Enzyme.configure({adapter: new Adapter()});
+
+const noop = () => {};
+
+it(`should replay button be pressed`, () => {
+  const handleReplayButtonClick = jest.fn();
+
+  const wrapper = shallow(
+      <AuthScreen
+        onReplayButtonClick={handleReplayButtonClick}
+        onSubmit={noop}
+      />
+  );
+
+  const replayButton = wrapper.find(`button.replay`);
+  replayButton.simulate(`click`);
+  expect(handleReplayButtonClick).toHaveBeenCalledTimes(1);
+});
